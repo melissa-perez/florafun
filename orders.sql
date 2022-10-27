71d0064cb6f3f216cf5fde79881e46c170b098a3
Melissa Perez <melissa-perez@users.noreply.github.com>
2022-10-26 20:57:10 -0400

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,28 +1,23 @@
 -- Create new order
 INSERT INTO
   `Orders` (order_date, order_quantity, total_sale_price)
-VALUES
-  (
-    :order_dateInput,
-    :order_quantityInput,
-    :total_sale_priceInput
-  ) -- View table uppon clicking Orders page
-SELECT
-  *
-FROM
-  Orders -- Update order
-UPDATE
-  `Orders`
-SET
-  order_date = :order_dateInput,
-  order_quantity = :order_quantityInput,
-  total_sale_price = :total_sale_priceInput
-WHERE
-  id = :order_ID_from_the_update_form -- Delete order
-DELETE FROM
-  Orders
-WHERE
-  id = :order_ID_selected_from_browse_order_page -- Create new order_item
+VALUES(:order_dateInput, :order_quantityInput, :total_sale_priceInput)
+
+-- View table uppon clicking Orders page
+SELECT * FROM Orders
+
+-- Update order
+UPDATE `Orders`
+SET order_date = :order_dateInput, order_quantity= :order_quantityInput, total_sale_price = :total_sale_priceInput
+WHERE id= :order_ID_from_the_update_form
+
+-- Delete order
+DELETE FROM Orders WHERE id = :order_ID_selected_from_browse_order_page
+
+
+
+
+-- Create new order_item
 INSERT INTO
   `Order_Items` (quantity)
 VALUES(:quantityInput)
@@ -37,4 +32,3 @@ WHERE id= :order_item_ID_from_the_update_form
 
 -- Delete order
 DELETE FROM Order_Items WHERE id = :order_item_ID_selected_from_browse_order_item_page
-
