93d7307f47434de32b2ff63c0338f280263712cd
springja <102645342+springja@users.noreply.github.com>
2022-11-15 19:57:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -194,3 +194,215 @@ app.post('/add-color-form', function (req, res) {
     }
   })
 })
+/*************************************
+  DISCOUNTS ROUTES
+**************************************/
+// Page to render for discounts READ
+app.get('/discounts', function (req, res) {
+    // Declare Query 1
+    let query1
+  
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.code === undefined) {
+      query1 = 'SELECT * FROM Discounts;'
+    }
+  
+    // If there is a query string, we assume this is a search, and return desired results
+    else {
+      query1 = `SELECT * FROM Discounts WHERE code LIKE "${req.query.code}%"`
+    }
+  
+    // Query 2 is the same in both cases
+    //let query2 = "SELECT * FROM Discounts;";
+  
+    // Run the 1st query
+    db.pool.query(query1, function (error, rows, fields) {
+      // Save the discounts
+      let discounts = rows
+  
+      // Run the second query
+      //db.pool.query(query2, (error, rows, fields) => {
+  
+      // Save the planets
+      //let planets = rows;
+      res.render('discounts.hbs', {
+        layout: 'index.hbs',
+        pageTitle: 'Discounts',
+        data: discounts,
+      })
+    })
+  })
+  // Page to render for discounts CREATE
+  app.post('/add-discount-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body
+  
+    // Capture NULL values
+    //let email = parseInt(data['input-email']);
+    //if (isNaN(email))
+    //{
+    //    email = 'NULL'
+    //}
+  
+    //let is_local = parseInt(data['input-is_local']);
+    //if (isNaN(is_local))
+    //{
+    //    is_local = 'NULL'
+    //}
+  
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Discounts (code, percent) VALUES ('${data['input-code']}', '${data['input-percent']}')`
+    db.pool.query(query1, function (error, rows, fields) {
+      // Check to see if there was an error
+      if (error) {
+        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+        console.log(error)
+        res.sendStatus(400)
+      }
+  
+      // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM Discounts and
+      // presents it on the screen
+      else {
+        res.redirect('/discounts')
+      }
+    })
+  })
+  // Page to render for discounts UPDATE
+  app.post('/update-discount-form', function (req, res) {
+    let data = req.body
+    //let discountID = parseInt(data.id);
+    //let deleteDiscounts = `DELETE FROM Discounts WHERE pid = ?`;
+    let updateDiscounts = `UPDATE Discounts SET code = '${data['input-code']}' , percent = '${data['input-percent']}' WHERE discount_id = '${data['input-discount_id']}'`
+    // Run the second query
+    db.pool.query(updateDiscounts, function (error, rows, fields) {
+      if (error) {
+        console.log(error)
+        res.sendStatus(400)
+      } else {
+        res.redirect('/discounts')
+      }
+    })
+  })
+  // Page to render for discounts DELETE
+  app.post('/delete-discount-form', function (req, res) {
+    let data = req.body
+    //let discountID = parseInt(data.id);
+    //let deleteDiscounts = `DELETE FROM Discounts WHERE pid = ?`;
+    let deleteDiscounts = `DELETE FROM Discounts WHERE discount_id = '${data['input-discount_id']}'`
+    // Run the second query
+    db.pool.query(deleteDiscounts, function (error, rows, fields) {
+      if (error) {
+        console.log(error)
+        res.sendStatus(400)
+      } else {
+        res.redirect('/discounts')
+      }
+    })
+  })
+  
+  /*************************************
+  PAYMENT METHODS ROUTES
+**************************************/
+// Page to render for payment_methods READ
+app.get('/payment_methods', function (req, res) {
+    // Declare Query 1
+    let query1
+  
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.type === undefined) {
+      query1 = 'SELECT * FROM Payment_Methods;'
+    }
+  
+    // If there is a query string, we assume this is a search, and return desired results
+    else {
+      query1 = `SELECT * FROM Payment_Methods WHERE type LIKE "${req.query.type}%"`
+    }
+  
+    // Query 2 is the same in both cases
+    //let query2 = "SELECT * FROM Payment_Methods;";
+  
+    // Run the 1st query
+    db.pool.query(query1, function (error, rows, fields) {
+      // Save the payment_methods
+      let payment_methods = rows
+  
+      // Run the second query
+      //db.pool.query(query2, (error, rows, fields) => {
+  
+      // Save the planets
+      //let planets = rows;
+      res.render('payment_methods.hbs', {
+        layout: 'index.hbs',
+        pageTitle: 'Payment_Methods',
+        data: payment_methods,
+      })
+    })
+  })
+  // Page to render for payment_methods CREATE
+  app.post('/add-payment_method-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body
+  
+    // Capture NULL values
+    //let email = parseInt(data['input-email']);
+    //if (isNaN(email))
+    //{
+    //    email = 'NULL'
+    //}
+  
+    //let is_local = parseInt(data['input-is_local']);
+    //if (isNaN(is_local))
+    //{
+    //    is_local = 'NULL'
+    //}
+  
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Payment_Methods (type) VALUES ('${data['input-type']}')`
+    db.pool.query(query1, function (error, rows, fields) {
+      // Check to see if there was an error
+      if (error) {
+        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+        console.log(error)
+        res.sendStatus(400)
+      }
+  
+      // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM Payment_Methods and
+      // presents it on the screen
+      else {
+        res.redirect('/payment_methods')
+      }
+    })
+  })
+  // Page to render for payment_methods UPDATE
+  app.post('/update-payment_method-form', function (req, res) {
+    let data = req.body
+    //let payment_methodID = parseInt(data.id);
+    //let deletePayment_Methods = `DELETE FROM Payment_Methods WHERE pid = ?`;
+    let updatePayment_Methods = `UPDATE Payment_Methods SET type = '${data['input-type']}' WHERE payment_method_id = '${data['input-payment_method_id']}'`
+    // Run the second query
+    db.pool.query(updatePayment_Methods, function (error, rows, fields) {
+      if (error) {
+        console.log(error)
+        res.sendStatus(400)
+      } else {
+        res.redirect('/payment_methods')
+      }
+    })
+  })
+  // Page to render for payment_methods DELETE
+  app.post('/delete-payment_method-form', function (req, res) {
+    let data = req.body
+    //let payment_methodID = parseInt(data.id);
+    //let deletePayment_Methods = `DELETE FROM Payment_Methods WHERE pid = ?`;
+    let deletePayment_Methods = `DELETE FROM Payment_Methods WHERE payment_method_id = '${data['input-payment_method_id']}'`
+    // Run the second query
+    db.pool.query(deletePayment_Methods, function (error, rows, fields) {
+      if (error) {
+        console.log(error)
+        res.sendStatus(400)
+      } else {
+        res.redirect('/payment_methods')
+      }
+    })
+  })
+  
