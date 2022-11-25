f347bcf6716095d8f5c1c22ac96ab04c067a87ab
Melissa Perez <peremeli@flip1.engr.oregonstate.edu>
2022-11-25 15:50:43 -0800

    change port number

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const PORT = 3421
+const PORT = 3423
 
 // Database
 const db = require('./database/db-connector')
