48d4a6102c2ccd24b6c8001bd5846c78cb6031b5
Melissa Perez <peremeli@flip2.engr.oregonstate.edu>
2022-11-26 23:03:57 -0800

    keep working on updateItem.js

--- public/js/updateItem.js
+++ public/js/updateItem.js
@@ -1,32 +1,38 @@
 const updateItemForm = document.getElementById('update-item-form')
 
 updateItemForm.addEventListener('submit', function (e) {
-  const itemID = document.getElementById('update-customer-select').value
+  const itemID = document.getElementById('update-item-select').value
   const newName = document.getElementById('update-name')
-  const newAddress = document.getElementById('update-address')
-  const newEmail = document.getElementById('update-email')
-  const newPhone = document.getElementById('update-phone')
+  const newSciName = document.getElementById('update-sci-name')
+  const newStock = document.getElementById('update-stock')
+  const newPrice = document.getElementById('update-price')
+  const newColorID = document.getElementById('update-color-select').value
+  const newSupplierID = document.getElementById('update-supplier-select').value
+  const newIndoor = document.getElementById('update-indoor-select').value
 
   const newNameValue = newName.value
-  const newAddressValue = newAddress.value
-  const newEmailValue = newEmail.value
-  const newPhoneValue = newPhone.value
+  const newSciNameValue = newSciName.value
+  const newStockValue = newStock.value
+  const newPriceValue = newPrice.value
 
   let data = {
-    id: customerID,
+    id: itemID,
     name: newNameValue,
-    address: newAddressValue,
-    email: newEmailValue,
-    phone: newPhoneValue,
+    sciName: newSciNameValue,
+    stock: newStockValue,
+    price: newPriceValue,
+    colorid: newColorID,
+    supplierid: newSupplierID,
+    indoor: newIndoor,
   }
 
   var xhttp = new XMLHttpRequest()
-  xhttp.open('PUT', '/update-customer-form', true)
+  xhttp.open('PUT', '/update-item-form', true)
   xhttp.setRequestHeader('Content-type', 'application/json')
 
   xhttp.onreadystatechange = () => {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      window.location = '/customers'
+      window.location = '/items'
     } else if (xhttp.readyState == 4 && xhttp.status != 200) {
       console.log('There was an error with the input.')
     }
@@ -49,10 +55,12 @@ function itemFill(data) {
   for (let i = 0; i < data.length; i++) {
     if (data[i].ID == itemID) {
       document.getElementById('update-name').value = data[i].Item
-      document.getElementById('update-sci-name').value = data[i]['Scientific name']
+      document.getElementById('update-sci-name').value =
+        data[i]['Scientific name']
       document.getElementById('update-stock').value = data[i].Stock
       document.getElementById('update-price').value = data[i].Price
-      document.getElementById('update-indoor-select').value = (data[i].Indoor == 'Yes' ? '1' : '0' )
+      document.getElementById('update-indoor-select').value =
+        data[i].Indoor == 'Yes' ? '1' : '0'
       document.getElementById('update-color-select').value = ''
       document.getElementById('update-supplier-select').value = ''
       break
