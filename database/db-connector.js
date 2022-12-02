ea58b51fb6bc389e0cc19166649d2f8ca070b5d1
Melissa Perez <peremeli@flip2.engr.oregonstate.edu>
2022-12-01 21:18:39 -0800

    add more sample data rows'

--- database/db-connector.js
+++ database/db-connector.js
@@ -2,7 +2,8 @@
 
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql')
-
+// Database hid credentials   VtoCorleone ON STACK OVERFLOW
+// https://stackoverflow.com/questions/57470659/how-to-hide-connection-details-for-mysql-database-in-node-js
 // Create a 'connection pool' using the provided credentials
 let pool = mysql.createPool({
   connectionLimit: 10,
